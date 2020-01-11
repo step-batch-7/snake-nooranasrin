d71d40aed45f3450d62c93b647b08f8a8b4ba7ea
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-01-11 17:36:26 +0530

    added a getter in class Game
    the getter will give all the informations to print the board
    extracted the classes into separated files

---
+++ game.js
@@ -0,0 +1,18 @@
+class Game {
+  constructor(snake, ghostSnake, food) {
+    this.snake = snake;
+    this.ghostSnake = ghostSnake;
+    this.food = food;
+  }
+
+  get currentStatus() {
+    const snake = { location: this.snake.location.slice() };
+    snake.type = this.snake.type;
+    snake.previousTail = this.snake.previousTail.slice();
+    const ghostSnake = { location: this.ghostSnake.location.slice() };
+    ghostSnake.type = this.ghostSnake.type;
+    ghostSnake.previousTail = this.ghostSnake.previousTail.slice();
+    const food = { location: [this.food.colId, this.food.rowId] };
+    return { snake, ghostSnake, food };
+  }
+}
---
+++ gameController.js
@@ -0,0 +1,111 @@
+const EAST = 0;
+const NORTH = 1;
+const WEST = 2;
+const SOUTH = 3;
+
+const NUM_OF_COLS = 100;
+const NUM_OF_ROWS = 60;
+
+const GRID_ID = 'grid';
+
+const getGrid = () => document.getElementById(GRID_ID);
+const getCellId = (colId, rowId) => colId + '_' + rowId;
+
+const getCell = (colId, rowId) =>
+  document.getElementById(getCellId(colId, rowId));
+
+const createCell = function(grid, colId, rowId) {
+  const cell = document.createElement('div');
+  cell.className = 'cell';
+  cell.id = getCellId(colId, rowId);
+  grid.appendChild(cell);
+};
+
+const createGrids = function() {
+  const grid = getGrid();
+  for (let y = 0; y < NUM_OF_ROWS; y++) {
+    for (let x = 0; x < NUM_OF_COLS; x++) {
+      createCell(grid, x, y);
+    }
+  }
+};
+
+const eraseTail = function(snake) {
+  let [colId, rowId] = snake.previousTail;
+  const cell = getCell(colId, rowId);
+  cell.classList.remove(snake.species);
+};
+
+const drawSnake = function(snake) {
+  snake.location.forEach(([colId, rowId]) => {
+    const cell = getCell(colId, rowId);
+    cell.classList.add(snake.species);
+  });
+};
+
+const drawFood = function(food) {
+  let [colId, rowId] = food.location;
+  const cell = getCell(colId, rowId);
+  cell.classList.add('food');
+};
+
+const handleKeyPress = snake => {
+  snake.turnLeft();
+};
+
+const moveAndDrawSnake = function(snake) {
+  snake.move();
+  eraseTail(snake);
+  drawSnake(snake);
+};
+
+const attachEventListeners = snake => {
+  document.body.onkeydown = handleKeyPress.bind(null, snake);
+};
+
+const initSnake = (direction, species) => {
+  const snakePosition = {
+    snake: [
+      [40, 25],
+      [41, 25],
+      [42, 25]
+    ],
+    ghost: [
+      [40, 30],
+      [41, 30],
+      [42, 30]
+    ]
+  };
+  return new Snake(snakePosition[species], new Direction(direction), species);
+};
+
+const setup = game => {
+  attachEventListeners(game.snake);
+  createGrids();
+  const status = game.currentStatus;
+  drawSnake(status.snake);
+  drawSnake(status.ghostSnake);
+  drawFood(status.food);
+};
+
+const animateSnakes = (snake, ghostSnake) => {
+  moveAndDrawSnake(snake);
+  moveAndDrawSnake(ghostSnake);
+};
+
+const randomlyTurnSnake = snake => {
+  let x = Math.random() * 100;
+  if (x > 50) {
+    snake.turnLeft();
+  }
+};
+
+const main = function() {
+  const snake = initSnake(EAST, 'snake');
+  const ghostSnake = initSnake(SOUTH, 'ghost');
+  const food = new Food(5, 5);
+  const game = new Game(snake, ghostSnake, food);
+  setup(game);
+  setInterval(animateSnakes, 200, snake, ghostSnake);
+  setInterval(randomlyTurnSnake, 500, ghostSnake);
+};
